4cf4ad63e61fdedc8b107d432de2f70cc9fdab2b
Vassilis Papavassilopoulos <teriyakivass@gmail.com>
2021-03-10 12:28:01 +0000

    syntax fix

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 bool callback(const PDU &pdu) {
     // Find the IP layer
     const IP &ip = pdu.rfind_pdu<IP>(); 
-    
+
     // Find the TCP layer
     const TCP &tcp = pdu.rfind_pdu<TCP>(); 
     cout << ip.src_addr() << ':' << tcp.sport() << " -> " 
@@ -16,7 +16,7 @@ bool callback(const PDU &pdu) {
 }
 
 int main() {
-    Sniffer sniffer("eth0")
-    sniffer.set_filter("p 80")
+    Sniffer sniffer("eth0");
+    sniffer.set_filter("p 80");
     sniffer.sniff_loop(callback);
 }
